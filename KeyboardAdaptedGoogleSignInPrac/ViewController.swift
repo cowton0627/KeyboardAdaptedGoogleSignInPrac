1fed2d023fbcf1e7174e42c838f91e196b71c1da
Charles Cheng <cowton0517@gmail.com>
2022-12-06 09:48:28 +0800

    Initial Commit

---
+++ KeyboardAdaptedGoogleSignInPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardAdaptedGoogleSignInPrac
+//
+//  Created by Chun-Li Cheng on 2022/12/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
